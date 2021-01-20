72b60687071253f9bf3ea1cde05e96ae6772f6a9
JeremyLeland <50390633+JeremyLeland@users.noreply.github.com>
2021-01-20 12:38:00 -0800

    Initial commit for Golf
    Basing this on the Physics Ammo Terrain demo from Three.js for the moment, making it fit into my framework

---
+++ games/Golf/game.js
@@ -0,0 +1,84 @@
+import * as THREE from "./libs/three.module.js" //'https://unpkg.com/three/build/three.module.js'
+
+export class Game {
+   static VERSION = 0
+
+   constructor() {
+      this.renderer = new THREE.WebGLRenderer()
+      this.renderer.setSize( window.innerWidth, window.innerHeight )
+      document.body.appendChild( this.renderer.domElement )
+
+      this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+
+      window.addEventListener('resize', () => this.resize(), false)
+
+      this.fpsUI = document.createElement('div')
+      this.fpsUI.style = "position: absolute; left: 100%; top: 100%; transform: translate(-14px, -14px); font: 10px sans-serif"
+      document.body.appendChild(this.fpsUI)
+      this.frames = 0
+      setInterval(() => this.updateFPS(), 1000)
+
+      this.versionUI = document.createElement('div')
+      this.versionUI.textContent = "Version: " + Game.VERSION
+      this.versionUI.style = "position: absolute; left: 2px; top: 100%; transform: translate(0, -14px); font: 10px sans-serif"
+      document.body.appendChild(this.versionUI)
+
+      this.keyBindings = {}
+      this.keyDown = {}
+      document.onkeydown = (e) => this.keyDownCallback(e)
+      document.onkeyup = (e) => this.keyUpCallback(e)
+   }
+
+   startGame() {
+      this.lastTime = new Date().getTime()
+      this.animate()
+   }
+
+   keyDownCallback(e) {
+      for (var k in this.keyBindings) {
+         if (e.keyCode == this.keyBindings[k]) {
+            this.keyDown[k] = true
+         }
+      }
+   }
+
+   keyUpCallback(e) {
+      for (var k in this.keyBindings) {
+         if (e.keyCode == this.keyBindings[k]) {
+            this.keyDown[k] = false
+         }
+      }
+   }
+
+   resize() {
+      this.renderer.setSize( window.innerWidth, window.innerHeight )
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+   }
+
+   animate() {
+      requestAnimationFrame( () => this.animate() )
+
+      const MAX_DT = 20
+
+      let now = new Date().getTime()
+
+      // Do multiple updates for long delays (so we don't miss things)
+      for (let dt = now - this.lastTime; dt > 0; dt -= MAX_DT) {
+         this.update(Math.min(dt, MAX_DT))
+      }
+
+      this.lastTime = now
+
+      this.render(this.renderer)
+      this.frames ++
+   }
+
+   updateFPS() {
+      this.fpsUI.textContent = this.frames
+      this.frames = 0
+   }
+
+   update(dt) {}
+   render(renderer) {}
+}
---
+++ games/Golf/golf.js
@@ -0,0 +1,103 @@
+import * as THREE from "./libs/three.module.js" //'https://unpkg.com/three/build/three.module.js'
+import { OrbitControls } from './libs/OrbitControls.js'
+import { Game } from "./game.js"
+
+export class Golf extends Game {
+   constructor() {
+      Game.VERSION = 0.01
+
+      super()
+
+      this.keyBindings = { }
+
+      this.scene = new THREE.Scene()
+      this.scene.background = new THREE.Color( 0xbfd1e5 )
+
+      this.camera.position.y = 40
+      this.camera.position.z = 40
+      this.camera.lookAt(0, 0, 0)
+
+      const controls = new OrbitControls( this.camera, this.renderer.domElement )
+
+      this.prepareTerrain()
+      this.prepareLights()
+
+      this.startGame()
+   }
+
+   prepareLights() {
+      const light = new THREE.DirectionalLight( 0xffffff, 1 );
+      light.position.set( 100, 100, 50 );
+      light.castShadow = true;
+      const dLight = 200;
+      const sLight = dLight * 0.25;
+      light.shadow.camera.left = - sLight;
+      light.shadow.camera.right = sLight;
+      light.shadow.camera.top = sLight;
+      light.shadow.camera.bottom = - sLight;
+
+      light.shadow.camera.near = dLight / 30;
+      light.shadow.camera.far = dLight;
+
+      light.shadow.mapSize.x = 1024 * 2;
+      light.shadow.mapSize.y = 1024 * 2;
+
+      this.scene.add( light );
+   }
+
+   prepareTerrain() {
+      const LEVEL_WIDTH = 100, LEVEL_HEIGHT = 100
+      const LEVEL_WIDTH_SEGMENTS = 128, LEVEL_HEIGHT_SEGMENTS = 128
+
+      const geometry = new THREE.PlaneBufferGeometry( LEVEL_WIDTH, LEVEL_HEIGHT, LEVEL_WIDTH_SEGMENTS - 1, LEVEL_HEIGHT_SEGMENTS - 1 )
+      geometry.rotateX( -Math.PI / 2 )
+
+      const heightData = this.generateHeight( LEVEL_WIDTH_SEGMENTS, LEVEL_HEIGHT_SEGMENTS, -2, 8 )
+
+      const vertices = geometry.attributes.position.array
+      for (let i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3) {
+         // j + 1 because we are modifying the Y component
+         vertices[ j + 1 ] = heightData[ i ]
+      }
+      geometry.computeVertexNormals()
+
+      const groundMaterial = new THREE.MeshPhongMaterial( { color: 0xC7C7C7 } );
+      const terrainMesh = new THREE.Mesh( geometry, groundMaterial );
+      terrainMesh.receiveShadow = true;
+      terrainMesh.castShadow = true;
+
+      this.scene.add( terrainMesh )
+   }
+
+   generateHeight(width, depth, minHeight, maxHeight) {
+      const size = width * depth;
+      const data = new Float32Array( size );
+
+      const hRange = maxHeight - minHeight;
+      const w2 = width / 2;
+      const d2 = depth / 2;
+      const phaseMult = 12;
+
+      let p = 0;
+
+      for ( let j = 0; j < depth; j ++ ) {
+         for ( let i = 0; i < width; i ++ ) {
+            const radius = Math.sqrt(
+               Math.pow( ( i - w2 ) / w2, 2.0 ) +
+                  Math.pow( ( j - d2 ) / d2, 2.0 ) );
+            const height = ( Math.sin( radius * phaseMult ) + 1 ) * 0.5 * hRange + minHeight;
+            data[ p ] = height;
+            p ++;
+         }
+      }
+
+      return data;
+   }
+
+   update(dt) {
+   }
+
+   render(renderer) {
+      renderer.render( this.scene, this.camera )
+   }
+}
