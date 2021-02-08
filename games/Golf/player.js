2951ff52cdeb5236378ecda2c67ad671b155bc1c
Jeremy Leland <jeremy.leland@gmail.com>
2021-02-08 10:03:21 -0800

    Simplify rewinding time for collisions
    Seems to work as well as what I had before, but it's a bit shorter and easier to read

--- games/Golf/player.js
+++ games/Golf/player.js
@@ -42,75 +42,60 @@ export class Player {
    }
 
    update(dt) {
-      //while (dt > 0) {
-         const lastX = this.x
-         const lastY = this.y
-         const lastDX = this.dx
-         const lastDY = this.dy
-         this.updatePosition(dt)
+      const lastX = this.x
+      const lastY = this.y
+      this.updatePosition(dt)
 
-         const segments = this.level.getSegmentsNear(this)
-         let closestSegment = null
-         let closestHitTime = 1.0
+      const segments = this.level.getSegmentsNear(this)
+      let closestSegment = null
+      let closestHitTime = 1.0
 
-         segments.forEach(s => {
-            // Ignore segment if we are outside of it
-            if (Math.abs(s.getDistanceFromSegmentBounds(this.x, this.y)) < this.radius) {
+      segments.forEach(s => {
+         // Ignore segment if we are outside of it
+         if (Math.abs(s.getDistanceFromSegmentBounds(this.x, this.y)) < this.radius) {
 
-               // Based on: https://www.gamasutra.com/view/feature/131790/simple_intersection_tests_for_games.php
-               const d0 = s.getDistanceFromInfiniteLine(lastX, lastY)
-               const d1 = s.getDistanceFromInfiniteLine(this.x, this.y)
+            // Based on: https://www.gamasutra.com/view/feature/131790/simple_intersection_tests_for_games.php
+            const d0 = s.getDistanceFromInfiniteLine(lastX, lastY)
+            const d1 = s.getDistanceFromInfiniteLine(this.x, this.y)
 
-               // Negative is "outside" of segment, positive is "inside" of segment
-               if (d0 < d1) {
-                  const hitTime = (d0 + this.radius) / ( d0 - d1 ) // normalized time
+            // Negative is "outside" of segment, positive is "inside" of segment
+            if (d0 < d1) {
+               const hitTime = (d0 + this.radius) / ( d0 - d1 ) // normalized time
 
-                  if (hitTime < closestHitTime) {
-                     closestSegment = s
-                     closestHitTime = hitTime
-                  }
+               if (hitTime < closestHitTime) {
+                  closestSegment = s
+                  closestHitTime = hitTime
                }
             }
-         })
-
-         if (closestHitTime < 0.0) {
-            // If we were already hitting something, nudge out of it
-            let dist = this.radius + closestSegment.getDistanceFromInfiniteLine(this.x, this.y)
-
-            if (dist > 0) {
-               let nudgeX = closestSegment.normalX * dist
-               let nudgeY = closestSegment.normalY * dist
-
-               this.x += nudgeX
-               this.y += nudgeY
-               this.dx = lastDX
-               this.dy = lastDY
-
-               this.applyBounce(closestSegment)
-               //this.applyFriction(closestSegment, dt)
-
-               // update position for rest of time?
-            }
          }
-         else if (closestHitTime < 1.0) {
-            // If we hit something, start over and only update until the hit
-            this.x = lastX
-            this.y = lastY
-            this.dx = lastDX
-            this.dy = lastDY
+      })
 
-            const partialDT = closestHitTime * dt
+      if (closestHitTime < 0.0) {
+         // If we were already hitting something, nudge out of it
+         let dist = this.radius + closestSegment.getDistanceFromInfiniteLine(this.x, this.y)
 
-            this.updatePosition(partialDT)
+         if (dist > 0) {
+            let nudgeX = closestSegment.normalX * dist
+            let nudgeY = closestSegment.normalY * dist
 
-            dt -= partialDT
+            this.x += nudgeX
+            this.y += nudgeY
 
             this.applyBounce(closestSegment)
             //this.applyFriction(closestSegment, dt)
 
-            this.updatePosition(dt)
+            // update position for rest of time?
          }
-      //}
+      }
+      else if (closestHitTime < 1.0) {
+         // If we hit something, rewind to point of collision, bounce, then update rest of way
+         this.updatePosition((closestHitTime - 1.0) * dt)
+
+         this.applyBounce(closestSegment)
+         //this.applyFriction(closestSegment, dt)
+
+         this.updatePosition((1.0 - closestHitTime) * dt)
+      }
    }
 
    draw(ctx, scrollX, scrollY) {
