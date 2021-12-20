9fc957cb60ad98ef052cf6f7339a1bf80f9c96d0
Jeremy Leland <jeremy.leland@gmail.com>
2021-12-19 19:32:32 -0800

    Putting new Jumper in place

---
+++ games/Jumper/src/Level.js
@@ -0,0 +1,154 @@
+import * as THREE from '../lib/three.module.js';
+
+const BLOCK_WIDTH = 1;
+const BLOCK_HEIGHT = 0.5;
+const BLOCK_LENGTH = 2;
+
+const SPAWN_Y = 5;
+
+export class Level {
+  title;
+  cols;
+  rows;
+  blocks;
+  mesh;
+
+  spawnPosition;
+
+  static async fromImageSrc( title, src ) {
+    const image = new Image();
+    image.src = src;
+
+    await image.decode();
+
+    return new Level( title, image );
+  }
+
+  constructor( title, image ) {
+    this.title = title;
+    this.cols = image.width;
+    this.rows = image.height;
+
+    this.spawnPosition = new THREE.Vector3(
+      ( this.cols / 2 ) * BLOCK_WIDTH, 
+      SPAWN_Y, 
+      ( this.rows - 0.5 ) * BLOCK_LENGTH
+    );
+
+    this.blocks = Array.from( getImageDataBuffer( image ), 
+      color => ( color & 0x00FFFFFF ) == 0 ? null : new THREE.Color( color ) 
+    );
+
+    this.mesh = getMesh( this.cols, this.rows, this.blocks );
+    this.mesh.scale.set( BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_LENGTH );
+
+    this.mesh.castShadow = false;
+    this.mesh.receiveShadow = true;
+  }
+
+  isSolidAt( x, z ) {
+    const col = Math.floor( x / BLOCK_WIDTH );
+    const row = Math.floor( z / BLOCK_LENGTH );
+    return 0 <= col && col < this.cols && 0 <= row && row < this.rows &&
+      this.blocks[ col + row * this.cols ];
+  }
+}
+
+function getMesh( cols, rows, blocks ) {
+  const positions = [];
+  const normals = [];
+  const colors = [];
+  const indices = [];
+  
+  let index = 0;
+  const addIndexAndColors = ( block ) => {
+    indices.push( index,     index + 1, index + 2 );
+    indices.push( index + 2, index + 1, index + 3 );
+    index += 4;
+
+    for ( let i = 0; i < 4; i ++ ) {
+      colors.push( block.b, block.g, block.r );   // colors are backwards for some reason
+    }
+  }
+
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      const block = blocks[ col + row * cols ];
+
+      if ( block ) {
+        // Top
+        [ 0, 1 ].forEach( z => {
+          [ 1, 0 ].forEach( x => {
+            positions.push( col + x, 0, row + z );
+            normals.push( 0, 1, 0 );
+          } );
+        });
+  
+        addIndexAndColors( block );
+
+        // Front
+        if ( row == rows - 1 || !blocks[ col + ( row + 1 ) * cols ] ) {
+          [ 0, -1 ].forEach( y => {
+            [ 1, 0 ].forEach( x => {
+              positions.push( col + x, y, row + 1 );
+              normals.push( 0, 0, 1 );
+            } );
+          });
+    
+          addIndexAndColors( block );
+        }
+
+        // Left
+        if ( col > ( cols / 2 ) && !blocks[ col - 1 + row * cols ] ) {
+          [ 0, -1 ].forEach( y => {
+            [ 1, 0 ].forEach( z => {
+              positions.push( col, y, row + z );
+              normals.push( -1, 0, 0 );
+            } );
+          });
+    
+          addIndexAndColors( block );
+        }
+
+        // Right
+        if ( col < ( cols / 2 ) && !blocks[ col + 1 + row * cols ] ) {
+          [ 0, -1 ].forEach( y => {
+            [ 0, 1 ].forEach( z => {
+              positions.push( col + 1, y, row + z );
+              normals.push( 1, 0, 0 );
+            } );
+          });
+    
+          addIndexAndColors( block );
+        }
+      }
+    }
+  }
+
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+  geometry.setAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
+  geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+  geometry.setIndex( indices );
+
+  const material = new THREE.MeshPhongMaterial( {
+    //color: 0xC7C7C7,
+    flatShading: true,
+    vertexColors: true,
+  } );
+
+  return new THREE.Mesh( geometry, material );
+}
+
+function getImageDataBuffer( image ) {
+  const canvas = document.createElement( 'canvas' );
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  const ctx = canvas.getContext( '2d' );
+  ctx.drawImage( image, 0, 0 );
+
+  return new Uint32Array(
+    ctx.getImageData( 0, 0, canvas.width, canvas.height ).data.buffer
+  );
+}
---
+++ games/Jumper/src/Player.js
@@ -0,0 +1,122 @@
+import * as THREE from '../lib/three.module.js';
+
+const SPEED = 0.01;
+const MAX_SPEED = 8;
+const ACCEL = 0.006;
+const SIDE_SPEED = 0.008;
+const JUMP_SPEED = 0.02;
+const SIZE = 0.4;
+const GRAVITY = -0.0001;
+const COLLISION_FUDGE = 0.3;
+const FALL_NO_RETURN = -0.4;
+const FALL_END = -40;
+const DETAIL = 40;
+
+const speedUI = document.getElementById( 'speed' );
+const titleUI = document.getElementById( 'title' );
+
+export class Player {
+  mesh = new THREE.Mesh(
+    new THREE.SphereGeometry( SIZE, DETAIL, DETAIL ),
+    new THREE.MeshPhongMaterial( {
+      color: 0xffffff, 
+      transparent: true, 
+      opacity: 0.8
+    } )
+  );
+
+  position = this.mesh.position;
+  velocity = new THREE.Vector3();
+
+  speed = 0;
+  #partialSpeed = 0;
+
+  completedLevel = false;
+  
+  constructor() {
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = false;
+  }
+
+  spawn( level ) {
+    this.position.copy( level.spawnPosition );
+    this.velocity.set( 0, 0, 0 );
+    this.speed = 0;
+    this.#partialSpeed = 0;
+    this.completedLevel = false;
+
+    titleUI.innerText = level.title;
+  }
+
+  update( { dt, level, keysPressed } ) {
+    if ( keysPressed.has( 'ArrowLeft' ) ) {
+      this.velocity.x = -SIDE_SPEED;
+    }
+    else if ( keysPressed.has( 'ArrowRight' ) ) {
+      this.velocity.x = SIDE_SPEED;
+    }
+    else {
+      this.velocity.x = 0;
+    }
+
+    this.velocity.y += GRAVITY * dt;
+
+    if ( keysPressed.has( 'ArrowUp' ) ) {
+      if ( this.#partialSpeed == 0 ) {
+        this.#partialSpeed = 1;
+      }
+      
+      this.#partialSpeed += ACCEL * dt;
+
+      if ( this.#partialSpeed > 1 ) {
+        this.speed = Math.min( this.speed + 1, MAX_SPEED );
+        this.#partialSpeed -= 1;
+      }
+    }
+    else if ( keysPressed.has( 'ArrowDown' ) ) {
+      if ( this.#partialSpeed == 0 ) {
+        this.#partialSpeed = -1;
+      }
+
+      this.#partialSpeed -= ACCEL * dt;
+
+      if (this.#partialSpeed < -1) {
+        this.speed = Math.max( this.speed - 1, 0 );
+        this.#partialSpeed += 1;
+      }
+    }
+    else {
+      this.#partialSpeed = 0;
+    }
+
+    this.velocity.z = -SPEED * this.speed;
+    speedUI.innerText = this.speed;
+    if ( this.speed > 0 ) {
+      titleUI.innerText = '';  
+    }
+
+    this.position.addScaledVector( this.velocity, dt );
+
+    if ( FALL_NO_RETURN < this.position.y && this.position.y < SIZE && 
+        [ -1, 1 ].some( x =>
+          [ -1, 1 ].some( z =>
+            level.isSolidAt( 
+              this.position.x + x * COLLISION_FUDGE, 
+              this.position.z + z * COLLISION_FUDGE
+            )
+          )
+        ) ) {
+      this.position.y = SIZE;
+      this.velocity.y = keysPressed.has( ' ' ) ? JUMP_SPEED : 0;
+
+      if ( this.position.z < SIZE ) {
+        this.position.z = SIZE;
+        this.completedLevel = true;
+        titleUI.innerText = 'Level Complete!';
+      }
+    }
+    else if ( this.position.y < FALL_END ) {
+      this.spawn( level );
+    }
+  }
+}
