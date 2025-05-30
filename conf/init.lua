08b9f7da1ea610d01662d12d1aeb2b6efbfa380d
Jeremy Leland <jeremy.leland@gmail.com>
2025-05-30 15:17:06 -0700

    Adding a decent start to an nvim config

---
+++ conf/init.lua
@@ -0,0 +1,153 @@
+-- ─── Bootstrap lazy.nvim ─────────────────────────────────────────────
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ─── Plugin Setup ────────────────────────────────────────────────────
+require("lazy").setup({
+  -- LSP base and installer
+  { "neovim/nvim-lspconfig" },
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = true,
+  },
+  { "williamboman/mason-lspconfig.nvim" },
+
+  -- Completion
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
+  },
+
+  -- Tree-sitter aware themes
+  { "catppuccin/nvim" },
+  { "folke/tokyonight.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Comments
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+    lazy = false
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
+
+  -- Rainbow brackets
+  { "HiPhish/rainbow-delimiters.nvim" },
+})
+
+-- ─── LSP Config ──────────────────────────────────────────────────────
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "ts_ls", "pyright" },
+  automatic_installation = true,
+})
+
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = capabilities,
+    })
+  end,
+})
+
+-- ─── Completion Config ───────────────────────────────────────────────
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args) luasnip.lsp_expand(args.body) end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+})
+
+-- ─── Treesitter Config ───────────────────────────────────────────────
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "lua", "javascript", "typescript", "python", "json" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+-- ─── TODO Comments Navigation ────────────────────────────────────────
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next TODO comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous TODO comment" })
+
+vim.keymap.set("n", "<C-_>", function()
+  require("Comment.api").toggle.linewise.current()
+end, { desc = "Toggle comment line" })
+
+vim.keymap.set("v", "<C-_>", function()
+  local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+  vim.api.nvim_feedkeys(esc, "nx", false)
+  require("Comment.api").toggle.linewise(vim.fn.visualmode())
+end, { desc = "Toggle comment selection" })
+
+
+-- Rainbow brackets
+local rainbow_delimiters = require('rainbow-delimiters')
+
+vim.g.rainbow_delimiters = {
+  strategy = {
+    [''] = rainbow_delimiters.strategy['global'],
+    vim = rainbow_delimiters.strategy['local'],
+  },
+  query = {
+    [''] = 'rainbow-delimiters',
+    lua = 'rainbow-blocks',
+  },
+  priority = {
+    [''] = 110,
+    lua = 210,
+  },
+}
+
+
+-- Other config
+
+vim.cmd.colorscheme( 'tokyonight' )
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.tabstop = 2 -- number of visual spaces per TAB
+vim.opt.shiftwidth = 2 -- number of spaces for autoindent
+vim.opt.expandtab = true -- convert tabs to spaces
